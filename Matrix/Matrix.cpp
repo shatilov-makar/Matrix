7a3fd82225b19e4b56aa113f5d5ec8b0036e8562
shatilov-makar <62944516+shatilov-makar@users.noreply.github.com>
2020-05-22 13:58:43 +0500

    commit3

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,26 +1,46 @@
 ﻿#include "Matrix2D.h"
 #include "Matrix3D.h"
-
-using namespace std;
 #include <iostream>
+
+void fillMatrix(MatrixBase& matrix)
+{
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix.size(); j++)
+        {
+            matrix.element(i, j) = matrix.size() * i + j + 1;
+        }
+    }
+}
 
 int main()
 {
     Matrix2D m2a, m2b;
-    cout << "m2a = m2b =" << endl << m2a << endl;
+    fillMatrix(m2a);
+    fillMatrix(m2b);
+    std::cout << "m2a == m2b:" << std::endl;
+    std::cout << m2a << std::endl;
+
     Matrix3D m3a, m3b;
-    cout << "m3a = m3b =" << endl << m3a << endl;
+    fillMatrix(m3a);
+    fillMatrix(m3b);
+    std::cout  << "m3a == m3b:" << std::endl;
+    std::cout << m3a << std::endl;
+
     m2a += m2b;
     m2b *= 3;
 
     m3a += m3b;
     m3b *= 3;
 
-    cout <<"m2a += m2b:"<<endl<< m2a << endl;
-    cout << "m2b *= 3:" << endl << m2b << endl;
-
-    cout << "m3a += m3b:" << endl << m3a << endl;
-    cout <<"m3b *= 3:" << endl<< m3b << endl;
+    std::cout << "m2a += m2b. m2a = " << std::endl;
+    std::cout << m2a << std::endl;
+    std::cout << "m2b *= 3. m2b = " << std::endl;
+    std::cout << m2b << std::endl;
+    std::cout << "m3a += m3b. m3a = " << std::endl;
+    std::cout << m3a << std::endl;
+    std::cout << "m3b *= 3. m3b = " << std::endl;
+    std::cout << m3b << std::endl;
 }
 
 
